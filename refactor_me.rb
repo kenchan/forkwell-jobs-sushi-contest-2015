2e6037cac6a22e25ffab795e579e0a26b132ad95
Kenichi TAKAHASHI <kenichi.taka@gmail.com>
2015-12-02 13:16:34 +0900

    テストを書きやすくするためにメソッド化

--- refactor_me.rb
+++ refactor_me.rb
@@ -1,13 +1,15 @@
-0 <= age or raise
+def age_to_label(age)
+  0 <= age or raise
 
-if 0 <= age && age <= 2
-  'baby'
-elsif 3 <= age && age <= 6
-  'little child'
-elsif 7 <= age && age <= 12
-  'child'
-elsif 13 <= age && age <= 18
-  'youth'
-else
-  'adult'
+  if 0 <= age && age <= 2
+    'baby'
+  elsif 3 <= age && age <= 6
+    'little child'
+  elsif 7 <= age && age <= 12
+    'child'
+  elsif 13 <= age && age <= 18
+    'youth'
+  else
+    'adult'
+  end
 end
