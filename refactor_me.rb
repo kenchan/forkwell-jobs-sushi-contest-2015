ac48f0930b3efc54c95614228a25ee9e27325292
Kenichi TAKAHASHI <kenichi.taka@gmail.com>
2015-12-02 13:31:24 +0900

    ifをcase-whenにリファクタリング

--- refactor_me.rb
+++ refactor_me.rb
@@ -3,13 +3,14 @@ require 'test/unit'
 def age_to_label(age)
   0 <= age or raise
 
-  if 0 <= age && age <= 2
+  case age
+  when 0..2
     'baby'
-  elsif 3 <= age && age <= 6
+  when 3..6
     'little child'
-  elsif 7 <= age && age <= 12
+  when 7..12
     'child'
-  elsif 13 <= age && age <= 18
+  when 13..18
     'youth'
   else
     'adult'
