e5c8ef9c69d0321f035d34f37a44945b6117b137
Kenichi TAKAHASHI <kenichi.taka@gmail.com>
2015-12-02 13:28:30 +0900

    一通りのテストを作成

--- refactor_me.rb
+++ refactor_me.rb
@@ -17,7 +17,49 @@ def age_to_label(age)
 end
 
 class MyTest < Test::Unit::TestCase
-  def test_0
-    assert_equal('', age_to_label(0))
+  def test_negative_age
+    assert_raise do
+      age_to_label(-1)
+    end
+  end
+
+  def test_baby_min
+    assert_equal('baby', age_to_label(0))
+  end
+
+  def test_baby_max
+    assert_equal('baby', age_to_label(2))
+  end
+
+  def test_little_child_min
+    assert_equal('little child', age_to_label(3))
+  end
+
+  def test_little_child_max
+    assert_equal('little child', age_to_label(6))
+  end
+
+  def test_child_min
+    assert_equal('child', age_to_label(7))
+  end
+
+  def test_child_max
+    assert_equal('child', age_to_label(12))
+  end
+
+  def test_youth_min
+    assert_equal('youth', age_to_label(13))
+  end
+
+  def test_youth_max
+    assert_equal('youth', age_to_label(18))
+  end
+
+  def test_adult
+    assert_equal('adult', age_to_label(19))
+  end
+
+  def test_immortality
+    assert_equal('adult', age_to_label(200))
   end
 end
